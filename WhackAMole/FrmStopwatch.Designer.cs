6d0a2c335a1014381d72bb8eff891eac21988f0f
Etendo <etendo123@gmail.com>
2026-07-24 02:29:28 -0600

    Finished the UI and added stop and start functionality to the timer.

---
+++ WhackAMole/FrmStopwatch.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WhackAMole
+{
+    partial class FrmStopwatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lblTimeElapsed = new Label();
+            btnStart = new Button();
+            btnStop = new Button();
+            btnReset = new Button();
+            tmrStopwatch = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // lblTimeElapsed
+            // 
+            lblTimeElapsed.AutoSize = true;
+            lblTimeElapsed.Location = new Point(83, 77);
+            lblTimeElapsed.Name = "lblTimeElapsed";
+            lblTimeElapsed.Size = new Size(38, 15);
+            lblTimeElapsed.TabIndex = 0;
+            lblTimeElapsed.Text = "label1";
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(65, 115);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 1;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(65, 154);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(75, 23);
+            btnStop.TabIndex = 2;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            btnReset.Location = new Point(65, 192);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(75, 23);
+            btnReset.TabIndex = 3;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            // 
+            // tmrStopwatch
+            // 
+            tmrStopwatch.Enabled = true;
+            tmrStopwatch.Interval = 1000;
+            // 
+            // FrmStopwatch
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(256, 450);
+            Controls.Add(btnReset);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Controls.Add(lblTimeElapsed);
+            Name = "FrmStopwatch";
+            Text = "Stopwatch";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTimeElapsed;
+        private Button btnStart;
+        private Button btnStop;
+        private Button btnReset;
+        private System.Windows.Forms.Timer tmrStopwatch;
+    }
+}
---
+++ WhackAMole/FrmStopwatch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Elijah Hodge
+ * CST - 250
+ * 07/24/2026
+ * Whack-A-Mole
+ * Activity 5
+ */
+
+namespace WhackAMole
+{
+    public partial class FrmStopwatch : Form
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public FrmStopwatch()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Click Event Handler for btnStart
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnStartClick(object sender, EventArgs e)
+        {
+            // Start the stopwatch
+            tmrStopwatch.Start();
+        }
+
+        /// <summary>
+        /// Click Event Handler for btnStop
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnStopClick(object sender, EventArgs e)
+        {
+            // Stop the stopwatch
+            tmrStopwatch.Stop();
+        }
+    }
+}
