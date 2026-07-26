17d5722223e14a7a1bc96fb5dc3f5844b8ba7b3f
Etendo <etendo123@gmail.com>
2026-07-26 16:22:32 -0600

    Implemented challenges.

--- WhackAMole/FrmStopwatch.Designer.cs
+++ WhackAMole/FrmStopwatch.Designer.cs
@@ -35,12 +35,22 @@
             btnReset = new Button();
             tmrStopwatch = new System.Windows.Forms.Timer(components);
             btnTarget = new Button();
+            btnBomb = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            lblLives = new Label();
+            label3 = new Label();
+            lblScore = new Label();
+            label4 = new Label();
+            lblMissedTargets = new Label();
+            label5 = new Label();
+            lblLevel = new Label();
             SuspendLayout();
             // 
             // lblTimeElapsed
             // 
             lblTimeElapsed.AutoSize = true;
-            lblTimeElapsed.Location = new Point(12, 9);
+            lblTimeElapsed.Location = new Point(357, 9);
             lblTimeElapsed.Name = "lblTimeElapsed";
             lblTimeElapsed.Size = new Size(38, 15);
             lblTimeElapsed.TabIndex = 0;
@@ -92,11 +102,112 @@
             btnTarget.UseVisualStyleBackColor = true;
             btnTarget.Click += BtnTargetClickEH;
             // 
+            // btnBomb
+            // 
+            btnBomb.Location = new Point(492, 195);
+            btnBomb.Name = "btnBomb";
+            btnBomb.Size = new Size(100, 80);
+            btnBomb.TabIndex = 5;
+            btnBomb.Text = "Bomb";
+            btnBomb.UseVisualStyleBackColor = true;
+            btnBomb.Click += btnBombClickEH;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(255, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Time Remaining:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Lives:";
+            // 
+            // lblLives
+            // 
+            lblLives.AutoSize = true;
+            lblLives.Location = new Point(54, 9);
+            lblLives.Name = "lblLives";
+            lblLives.Size = new Size(38, 15);
+            lblLives.TabIndex = 8;
+            lblLives.Text = "label3";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(130, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Score:";
+            // 
+            // lblScore
+            // 
+            lblScore.AutoSize = true;
+            lblScore.Location = new Point(175, 9);
+            lblScore.Name = "lblScore";
+            lblScore.Size = new Size(38, 15);
+            lblScore.TabIndex = 10;
+            lblScore.Text = "label4";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(439, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(82, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Missed Moles:";
+            // 
+            // lblMissedTargets
+            // 
+            lblMissedTargets.AutoSize = true;
+            lblMissedTargets.Location = new Point(527, 9);
+            lblMissedTargets.Name = "lblMissedTargets";
+            lblMissedTargets.Size = new Size(38, 15);
+            lblMissedTargets.TabIndex = 12;
+            lblMissedTargets.Text = "label5";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(660, 9);
+            label5.Name = "label5";
+            label5.Size = new Size(80, 15);
+            label5.TabIndex = 13;
+            label5.Text = "Current Level:";
+            // 
+            // lblLevel
+            // 
+            lblLevel.AutoSize = true;
+            lblLevel.Location = new Point(746, 9);
+            lblLevel.Name = "lblLevel";
+            lblLevel.Size = new Size(38, 15);
+            lblLevel.TabIndex = 14;
+            lblLevel.Text = "label6";
+            // 
             // FrmStopwatch
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(840, 523);
+            Controls.Add(lblLevel);
+            Controls.Add(label5);
+            Controls.Add(lblMissedTargets);
+            Controls.Add(label4);
+            Controls.Add(lblScore);
+            Controls.Add(label3);
+            Controls.Add(lblLives);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnBomb);
             Controls.Add(btnTarget);
             Controls.Add(btnReset);
             Controls.Add(btnStop);
@@ -104,6 +215,7 @@
             Controls.Add(lblTimeElapsed);
             Name = "FrmStopwatch";
             Text = "Stopwatch";
+            Click += FrmClickEH;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -116,5 +228,15 @@
         private Button btnReset;
         private System.Windows.Forms.Timer tmrStopwatch;
         private Button btnTarget;
+        private Button btnBomb;
+        private Label label1;
+        private Label label2;
+        private Label lblLives;
+        private Label label3;
+        private Label lblScore;
+        private Label label4;
+        private Label lblMissedTargets;
+        private Label label5;
+        private Label lblLevel;
     }
 }
--- WhackAMole/FrmStopwatch.cs
+++ WhackAMole/FrmStopwatch.cs
@@ -20,28 +20,54 @@ namespace WhackAMole
 {
     public partial class FrmStopwatch : Form
     {
-        // Class level variable to hold the timers time
-        TimeSpan timeElapsed = new TimeSpan();
+        // Class level variable to hold the timer's time
+        TimeSpan timeElapsed = TimeSpan.FromMinutes(1);
         // Create a new Random object to generate numbers
         Random random = new Random();
+        // Class level variable to hold the score
+        int score = 0;
+        // Class level variable to hold the lives
+        int lives = 3;
+        // Class level variable to hold the current level
+        int currLevel = 1;
+        // Class level variable to accumulate for button movement
+        int moveAccumulator = 0;
+        // Class level variable to control how often the buttons move depending on the level
+        int moveInterval = 3000;
+        // Class level variable to hold a minimum points for the player to earn an extra life so that they can't just click the target repeatedly to earn infinite lives
+        int pointsReachedForExtraLife = 0;
+        // Class level variable to hold how many targets the player missed
+        int targetsMissed = 0;
 
         /// <summary>
-        /// Default constructor
+        /// Constructor for FrmStopwatch
         /// </summary>
         public FrmStopwatch()
         {
             InitializeComponent();
+
+            // Set the score label to show the initial score
+            lblScore.Text = score.ToString();
+            // Set the lives label to show the initial lives
+            lblLives.Text = lives.ToString();
+            // Set the level label to show the initial level
+            lblLevel.Text = currLevel.ToString();
+            // Set the missed targets label to show the initial missed targets
+            lblMissedTargets.Text = targetsMissed.ToString();
+
+            // Set timer interval to fixed value for smoothness
+            tmrStopwatch.Interval = 100;
         }
 
         /// <summary>
         /// Click Event Handler for btnStart
         /// Starts the timer
         /// </summary>
-        /// <param name="sender"></param>
+        /// <param name="sener"></param>
         /// <param name="e"></param>
-        private void BtnStartClickEH(object sender, EventArgs e)
+        private void BtnStartClickEH(object sener, EventArgs e)
         {
-            // Start the stopwatch
+            // Start the timer
             tmrStopwatch.Start();
         }
 
@@ -53,7 +79,7 @@ namespace WhackAMole
         /// <param name="e"></param>
         private void BtnStopClickEH(object sender, EventArgs e)
         {
-            // Stop the stopwatch
+            // Stop the timer
             tmrStopwatch.Stop();
         }
 
@@ -66,24 +92,88 @@ namespace WhackAMole
         /// <param name="e"></param>
         private void TmrStopwatchTickEH(object sender, EventArgs e)
         {
-            // Get the interval from tmrStopwatch
+            // Always use the fixed interval
             int interval = tmrStopwatch.Interval;
-            // Add the timers interval to timeElapsed
-            timeElapsed = timeElapsed.Add(TimeSpan.FromMilliseconds(interval));
-            // Show the timeElapsed on the label
-            lblTimeElapsed.Text = timeElapsed.ToString();
-            if (timeElapsed.TotalSeconds % 3 == 0)
+            // Subtract the timer's interval from timeElapsed
+            timeElapsed = timeElapsed.Subtract(TimeSpan.FromMilliseconds(interval));
+            // Accumulate time for button movement
+            moveAccumulator += interval;
+            // If timeElapsed is less than or equal to zero, stop the timer
+            if (timeElapsed <= TimeSpan.Zero)
             {
+                // Set the time to zero
+                timeElapsed = TimeSpan.Zero;
+                // Show the time elapsed on the label with the correct format
+                lblTimeElapsed.Text = timeElapsed.ToString(@"hh\:mm\:ss");
+                // Show a message box with the final score
+                tmrStopwatch.Stop();
+
+                // Show a message box with the final score and level reached
+                MessageBox.Show("Level complete! Your current score is: " + score.ToString(), "Good Job", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Increment the level
+                currLevel++;
+                // Update the level label
+                lblLevel.Text = currLevel.ToString();
+                // Calculate the move interval based on the current level
+                moveInterval = 3000 - (currLevel - 1) * 400;
+                // Ensure the move interval does not go below 600 ms
+                if (moveInterval < 600) moveInterval = 600;
+                // Reset move accumulator
+                moveAccumulator = 0;
+                // Reset timeElapsed to 1 minute
+                timeElapsed = TimeSpan.FromMinutes(1);
+                // Show the reset time on the label with the correct format
+                lblTimeElapsed.Text = timeElapsed.ToString(@"hh\:mm\:ss");
+                // Start the timer again for the next level
+                tmrStopwatch.Start();
+            }
+            // Show the timeElapsed in lblTime with the correct format
+            lblTimeElapsed.Text = timeElapsed.ToString(@"hh\:mm\:ss");
+            // Move the target and bomb buttons at the current move interval
+            if (timeElapsed.TotalSeconds > 0 && moveAccumulator >= moveInterval)
+            {
+                // Reset the move accumulator
+                moveAccumulator = 0;
                 // Select a new location for the top of btnTarget
-                // Randomly generate a location for the top of the button
-                // between 0 and the form height minus the button height
                 btnTarget.Top = random.Next(0, this.Height - btnTarget.Height);
                 // Select a new location for the left side of btnTarget
                 btnTarget.Left = random.Next(0, this.Width - btnTarget.Width);
                 // Get random numbers for the RGB color for the button
                 btnTarget.BackColor = Color.FromArgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
-                // Set the target to be visible
-                btnTarget.Visible = true;
+                // If we can't see the target, it means the player did get it
+                if (btnTarget.Visible == false)
+                {
+                    // Set the target to be visible
+                    btnTarget.Visible = true;
+                }
+                // If the target was already visible, it means the player missed it, so increment the missed target count
+                else
+                {
+                    // If the target was already visible, it means the player missed it, so increment the missed target count
+                    targetsMissed++;
+                    // Update the missed targets label
+                    lblMissedTargets.Text = targetsMissed.ToString();
+
+                    // Check if the player has missed 5 targets, if so decrement lives and reset the missed target count
+                    if (targetsMissed >= 5)
+                    {
+                        // Reset the missed target count
+                        targetsMissed = 0;
+                        // Update the missed targets label
+                        lblMissedTargets.Text = targetsMissed.ToString();
+                        // Call the decrement lives method to penalize the player for missing too many targets
+                        DecrementLives();
+                    }
+                }
+
+                // Select a new location for the top of btnBomb
+                btnBomb.Top = random.Next(0, this.Height - btnBomb.Height);
+                // Select a new location for the left side of btnBomb
+                btnBomb.Left = random.Next(0, this.Width - btnBomb.Width);
+                // Get random numbers for the RGB color for the button
+                btnBomb.BackColor = Color.FromArgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+                // Set the bomb to be visible
+                btnBomb.Visible = true;
             }
         } // End of TmrStopwatchTickEH
 
@@ -97,10 +187,32 @@ namespace WhackAMole
         {
             // Stop the timer
             tmrStopwatch.Stop();
-            // Reset elapsedTime
-            timeElapsed = new TimeSpan();
+            // Reset timeElapsed to 1 minute
+            timeElapsed = TimeSpan.FromMinutes(1);
             // Show the reset time on the label
-            lblTimeElapsed.Text = timeElapsed.ToString();
+            lblTimeElapsed.Text = timeElapsed.ToString(@"hh\:mm\:ss");
+            // Reset the score
+            score = 0;
+            // Update the score label
+            lblScore.Text = score.ToString();
+            // Reset the lives
+            lives = 3;
+            // Update the lives label
+            lblLives.Text = lives.ToString();
+            // Reset the level
+            currLevel = 1;
+            // Update the level label
+            lblLevel.Text = currLevel.ToString();
+            // Set the missed targets count to zero
+            targetsMissed = 0;
+            // Set the missed targets label to show the initial missed targets
+            lblMissedTargets.Text = targetsMissed.ToString();
+            // Reset timer interval to default
+            tmrStopwatch.Interval = 100;
+            // Reset move interval
+            moveInterval = 3000;
+            // Reset the accumulator
+            moveAccumulator = 0;
         }
 
         /// <summary>
@@ -112,6 +224,86 @@ namespace WhackAMole
         {
             // Hide the target
             btnTarget.Visible = false;
+            // Increment the score
+            score++;
+            // Update the score label
+            lblScore.Text = score.ToString();
+            // Set the missed targets count to zero
+            targetsMissed = 0;
+            // Update the missed targets label
+            lblMissedTargets.Text = targetsMissed.ToString();
+
+            // Check if the score is a multiple of 50, increment lives left if the player hasn't already earned an extra life for that score
+            if (score % 50 == 0 && pointsReachedForExtraLife < score)
+            {
+                // Increment lives
+                lives++;
+                // Update the lives label
+                lblLives.Text = lives.ToString();
+                // Update the pointsReachedForExtraLife to the current score to prevent multiple lives being earned for the same score
+                pointsReachedForExtraLife = score;
+            }
+        }
+
+        /// <summary>
+        /// Click Event Handler for FrmStopwatch to penalize the player for clicking anywhere on the form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FrmClickEH(object sender, EventArgs e)
+        {
+            // Check if the score is greater than 0 before decrementing
+            if (score > 0)
+            {
+                // Decrement the score
+                score--;
+                // Update the score label
+                lblScore.Text = score.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Click Event Handler for btnBomb to hide the bomb and decrement the player's lives
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBombClickEH(object sender, EventArgs e)
+        {
+            // Hide the bomb button
+            btnBomb.Visible = false;
+            // Call the decrement lives method to decrement the players lives
+            DecrementLives();
+        }
+
+        /// <summary>
+        /// Method to decrement the player's lives and check for game over condition
+        /// </summary>
+        private void DecrementLives()
+        {
+            // Check if the player has more than 1 life left before decrementing
+            if (lives > 1)
+            {
+                // Decrement lives
+                lives--;
+                // Update the lives label
+                lblLives.Text = lives.ToString();
+            }
+            else
+            {
+                // Decrement lives to 0
+                lives--;
+                // Update the lives label
+                lblLives.Text = lives.ToString();
+                // Set timeElapsed to zero and stop the timer
+                timeElapsed = TimeSpan.Zero;
+                // Update the time label
+                tmrStopwatch.Stop();
+                // Show the time elapsed on the label
+                lblTimeElapsed.Text = timeElapsed.ToString(@"hh\:mm\:ss");
+
+                // Show a message box with the final score
+                MessageBox.Show("Game over! Your final score is " + score.ToString() + " and your highest level reached is level " + currLevel.ToString(), "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
